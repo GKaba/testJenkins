8c9eac4b71adddae450103b893d7ed6c4f3107b1
g.kaba <gorgekaba@gmail.com>
2020-12-04 13:18:47 -0500

    test webhook

--- src/jenkinsexo/JenkinsExo.java
+++ src/jenkinsexo/JenkinsExo.java
@@ -17,7 +17,9 @@ public class JenkinsExo {
     public static void main(String[] args) {
         System.out.println("Bonjour "+ args[1]+","+args[0]);
         System.out.println("Au revoir");
+        System.out.println("test webhook");
         System.out.println("merci");
+        
     }
     
 }
