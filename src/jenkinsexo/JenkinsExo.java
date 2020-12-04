d545052204698e078f3da2e54a5673f9fd5d9865
g.kaba <gorgekaba@gmail.com>
2020-12-04 12:30:25 -0500

    change message

--- src/jenkinsexo/JenkinsExo.java
+++ src/jenkinsexo/JenkinsExo.java
@@ -15,7 +15,7 @@ public class JenkinsExo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bonjour"+ args[1]+","+args[0]);
+        System.out.println("Bonjour "+ args[1]+","+args[0]);
     }
     
 }
