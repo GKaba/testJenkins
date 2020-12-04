7d16797a8b975087f6a51fe32ad3815ea506a179
g.kaba <gorgekaba@gmail.com>
2020-12-04 12:52:13 -0500

    change message

--- src/jenkinsexo/JenkinsExo.java
+++ src/jenkinsexo/JenkinsExo.java
@@ -16,6 +16,8 @@ public class JenkinsExo {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour "+ args[1]+","+args[0]);
+        System.out.println("Au revoir");
+        System.out.println("merci");
     }
     
 }
